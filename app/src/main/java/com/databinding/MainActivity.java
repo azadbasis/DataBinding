877af6cce96987fc1ee6037477c462faa827cdf4
Azhar <azadbasis@gmail.com>
2019-05-27 00:36:07 +0600

    Object Binding

--- app/src/main/java/com/databinding/MainActivity.java
+++ app/src/main/java/com/databinding/MainActivity.java
@@ -11,10 +11,34 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityMainBinding binding= DataBindingUtil.setContentView(this,R.layout.activity_main);
-        binding.tv1.setText("AZHAR");
-        binding.tv2.setText("ANWAR");
-        binding.tv3.setText("ANIS");
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.setAStudent(new Student("Azharul",38));
 
+    }
+
+    public class Student{
+        private String name;
+        private int age;
+
+        public Student(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
