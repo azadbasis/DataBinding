597505124c74ac2bb9886f3b52d9bd93ca9adf30
Azhar <azadbasis@gmail.com>
2019-05-26 23:55:13 +0600

    Initial commit

---
+++ app/src/main/java/com/databinding/MainActivity.java
@@ -0,0 +1,20 @@
+package com.databinding;
+
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.databinding.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityMainBinding binding= DataBindingUtil.setContentView(this,R.layout.activity_main);
+        binding.tv1.setText("AZHAR");
+        binding.tv2.setText("ANWAR");
+        binding.tv3.setText("ANIS");
+        binding.tv4.setText("AMINA");
+    }
+}
